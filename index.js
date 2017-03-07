65011db9148f77b821331ed24417e42901d74b28
martinsvb <svoobdamartin@centrum.cz>
2017-03-07 21:42:14 +0100

    Move index.html to root

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var server_port = process.env.PORT || 3000;
 app.use(express.static(__projectRoot));
 
 app.get('/', function (req, res) {
-    res.sendFile('/build/index.html');
+    res.sendFile('/index.html');
 });
 
 app.listen(server_port);
